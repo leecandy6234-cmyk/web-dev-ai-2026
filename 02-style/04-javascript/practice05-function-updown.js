45185a37105ec88428c175e32ab63f9a0d1cf05b
leecandy6234-cmyk <leecandy6234@gmail.com>
2026-02-04 15:42:18 +0900

    2026-02-04_3-이은경

--- 02-style/04-javascript/practice05-function-updown.js
+++ 02-style/04-javascript/practice05-function-updown.js
@@ -9,31 +9,45 @@ let count = 0;
 let count_2 = 0;
 let count_3 = 0;
 //취소버튼 => nall인데 이때 종료 if (a === null) break;
+//alert(`공지`) >>>>
 while (true) {
   count++;
   const a = prompt(`1부터 ${x}까지의 숫자를 입력해주세요`);
+  if (a === "") {
+    alert(`입력하세요!`);
+  }
 
   if (a === null) {
+    //const b = prompt(`포기하시려면 다시한번 취소버튼를 누르세요`);
     console.log(`포기하셨습니다!`);
+    alert(`포기하셨습니다!`);
     break;
   }
+  /*if (b === null) {
+    console.log("포기하셨습니다!");
+    alert(`포기하셨습니다!`);
+    break;
+  } else if (b !== null) {
+  }*/
+
   console.log(`입력한 값은 ${a}입니다`);
   if (isNaN(a)) {
     const a = prompt("숫자만 입력해주세요");
     count_2++;
   } else if (a === X.toString()) {
     count_3 = count - count_2;
+    alert("성공!");
     console.log(
       `성공!  잘못입력한 값 ${count_2}번과 ${count_3}번의 시도로 총 ${count}번의 입력끝에 성공하셨습니다`,
     );
 
     break;
   } else if (a > X.toString()) {
-    const a = prompt("a가 정답 보다 더 큽니다. 다시입력하세요");
+    alert("a가 정답 보다 더 큽니다. 다시입력하세요");
   } else if (a < X.toString()) {
-    const a = prompt("a가 정답 보다 더 작습니다. 다시입력하세요");
+    alert("a가 정답 보다 더 작습니다. 다시입력하세요");
   } else if (a !== X.toString()) {
-    const a = prompt("틀렸습니다. 다시입력하세요");
+    alert("틀렸습니다. 다시입력하세요");
   }
 }
 
