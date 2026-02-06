256fe75074e867a3583d7b33e441f8f5a73a8ecb
leecandy6234-cmyk <leecandy6234@gmail.com>
2026-02-06 14:20:46 +0900

    2026-02-06_02-이은경

--- 04-javascript/practice07-lotto.js
+++ 04-javascript/practice07-lotto.js
@@ -18,44 +18,57 @@ let x5 = Math.ceil(Math.random() * a);
 let x6 = Math.ceil(Math.random() * a);
 //console.log(x6);
 
+let x7 = Math.ceil(Math.random() * a); //보너스!
+//console.log(x7);
+
 //중복 X
 
 while (true) {
-  if (x === x2 || x === x3 || x === x4 || x === x5 || x === x6) {
-    console.log("중복, 재실행");
+  if (x === x2 || x === x3 || x === x4 || x === x5 || x === x6 || x === x7) {
+    //console.log("중복, 재실행");
     x = Math.ceil(Math.random() * a);
-    console.log(`1번은 ${x}`);
-  } else if (x2 === x3 || x2 === x4 || x2 === x5 || x2 === x6) {
-    console.log("중복, 재실행");
+    //console.log(`1번은 ${x}`);
+  } else if (x2 === x3 || x2 === x4 || x2 === x5 || x2 === x6 || x2 === x7) {
+    //console.log("중복, 재실행");
     x2 = Math.ceil(Math.random() * a);
-    console.log(`2번은 ${x2}`);
-  } else if (x3 === x4 || x3 === x5 || x3 === x6) {
-    console.log("중복, 재실행");
+    //console.log(`2번은 ${x2}`);
+  } else if (x3 === x4 || x3 === x5 || x3 === x6 || x3 === x7) {
+    //console.log("중복, 재실행");
     x3 = Math.ceil(Math.random() * a);
-    console.log(`3번은 ${x3}`);
-  } else if (x4 === x5 || x4 === x6) {
-    console.log("중복, 재실행");
+    //console.log(`3번은 ${x3}`);
+  } else if (x4 === x5 || x4 === x6 || x4 === x7) {
+    //console.log("중복, 재실행");
     x4 = Math.ceil(Math.random() * a);
-    console.log(`4번은 ${x4}`);
-  } else if (x5 === x6) {
-    console.log("중복, 재실행");
+    //console.log(`4번은 ${x4}`);
+  } else if (x5 === x6 || x5 === x7) {
+    //console.log("중복, 재실행");
     x5 = Math.ceil(Math.random() * a);
-    console.log(`5번은 ${x5}`);
+    //console.log(`5번은 ${x5}`);
+  } else if (x6 === x7) {
+    //console.log("중복, 재실행");
+    x6 = Math.ceil(Math.random() * a);
+    //console.log(`6번은 ${x6}`);
   } else {
+    console.log(`로또 번호는 ${x}, ${x2}, ${x3}, ${x4}, ${x5}, ${x6} + ${x7}`);
     break;
   }
-} //최종적으로 나온 숫자
+}
+
+//최종적으로 나온 숫자
+/*
 console.log(x);
 console.log(x2);
 console.log(x3);
 console.log(x4);
 console.log(x5);
 console.log(x6);
+console.log(x7);
+*/
 
-//입력 받은 값
+//입력 받은 값-숫자만 가능하게
 let p;
 while (true) {
-  p = prompt("1부터 45까지 중복없이 6번을 입력해 주세요");
+  p = prompt("1부터 45까지 중복없이 7번을 입력해 주세요");
   if (isNaN(p)) {
     alert("다시 입력하세요");
   } else if (p === "") {
@@ -63,14 +76,16 @@ while (true) {
   } else if (p === null) {
     alert("다시 입력하세요");
   } else {
-    console.log(p);
+    console.log(`입력한 값은 ${p}입니다.`);
     break;
   }
 }
 
-console.log(p);
+//console.log(p);
 
-//1부터 a까지만 입력
+//1부터 a까지만 입력--아직 구상중...
+
+//a를 0까지 카운트
 while (true) {
   a--;
   console.log(`a의 카운트는 현재 ${a}입니다.`);
